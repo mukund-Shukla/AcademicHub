29a8dd30e7ce7da6cfd20b1c18112babe91a359d
mukund-Shukla <mukundshukla2003@gmail.com>
2024-01-16 02:26:25 +0530

    sidebar and navbar added

--- academihub/app/(dashboard)/(routes)/page.tsx
+++ academihub/app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,12 @@
+
+import { UserButton } from "@clerk/nextjs"
+
 export default function Home() {
   return (
     <div>
-      <p>This is a protected page</p>
+        <UserButton
+          afterSignOutUrl="/"
+        />
    </div>
   )
 } 
---
+++ academihub/app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,5 @@
+export const Navbar=()=>{
+    return(
+        <div className="p-4 border-b h-full flex items-center bg-white shadow-sm"></div>
+    )
+}
